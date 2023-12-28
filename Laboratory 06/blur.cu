cb565897bd7d8bcc8a7549c3ee0bb67b2d96981e
Fabrizio Mattos <fmattos@unsa.edu.pe>
2023-12-28 12:24:24 -0500

    First push

--- Laboratory 06/blur.cu
+++ Laboratory 06/blur.cu
@@ -10,6 +10,26 @@
 #define BLUR_SIZE 4
 using namespace cv;
 __global__
+void blur_grayKernel(unsigned char* in, unsigned char* out, int w, int h) {
+    int Col = blockIdx.x * blockDim.x + threadIdx.x;
+    int Row = blockIdx.y * blockDim.y + threadIdx.y;
+    if (Col < w && Row < h) {
+        int pixVal = 0;
+        int pixels = 0;
+        for (int blurRow = -BLUR_SIZE; blurRow < BLUR_SIZE + 1; ++blurRow) {
+            for (int blurCol = -BLUR_SIZE; blurCol < BLUR_SIZE + 1; ++blurCol) {
+                int curRow = Row + blurRow;
+                int curCol = Col + blurCol;
+                if (curRow > -1 && curRow < h && curCol > -1 && curCol < w) {
+                    pixVal += in[curRow * w + curCol];
+                    pixels++;
+                }
+            }
+        }
+        out[Row * w + Col] = (unsigned char)(pixVal / pixels);
+    }
+}
+__global__
 void blurKernel(unsigned char* in, unsigned char* out, int w, int h) {
     int Col = blockIdx.x * blockDim.x + threadIdx.x;
     int Row = blockIdx.y * blockDim.y + threadIdx.y;
